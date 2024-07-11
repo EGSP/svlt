32e828c35f1de2ef3d0aeaabdef7da0d62c98ebd
GuZa123123 <guza6142@gmail.com>
2024-07-11 09:38:50 +0300

    fixwd cpopy button

--- myapp/src/lib/components/CopyButton.svelte
+++ myapp/src/lib/components/CopyButton.svelte
@@ -1,25 +1,40 @@
 <script lang="ts">
-	import InlineButton from "./InlineButton.svelte";
-    import CarbonCopy from '$lib/icons/CarbonCopy.svelte';
-	
-    export let feedbackTimeout: number = 2000
-    export let valueToCopy: string;
+	import InlineButton from './InlineButton.svelte';
+	import CarbonCopy from '$lib/icons/CarbonCopy.svelte';
 
-    let iconDescription = 'Copy'
+	export let feedbackTimeout: number = 2000;
+	export let valueToCopy: string;
 
-    function handleClick() {
-        // writeText(valueToCopy);
-        
-        let temporary_iconDescription = iconDescription.slice()
-        iconDescription = 'Copied!'
-        setTimeout(() => {
-            iconDescription = temporary_iconDescription
-        }, feedbackTimeout)
-    }
+	let iconDescription = 'Copy';
+
+	function unsecuredCopyToClipboard(text: string) {
+		const textArea = document.createElement('textarea');
+		textArea.value = text;
+		document.body.appendChild(textArea);
+		textArea.focus();
+		textArea.select();
+		try {
+			document.execCommand('copy');
+		} catch (err) {
+			console.error('Unable to copy to clipboard', err);
+		}
+		document.body.removeChild(textArea);
+	}
+
+	function handleClick() {
+		
+		if (window.isSecureContext && navigator.clipboard) {
+			navigator.clipboard.writeText(valueToCopy);
+		} else {
+			unsecuredCopyToClipboard(valueToCopy);
+		}
+
+		let temporary_iconDescription = iconDescription.slice();
+		iconDescription = 'Copied!';
+		setTimeout(() => {
+			iconDescription = temporary_iconDescription;
+		}, feedbackTimeout);
+	}
 </script>
 
-
-
-
-<InlineButton icon={CarbonCopy} iconDescription={iconDescription} 
-on:click={handleClick}/>
+<InlineButton icon={CarbonCopy} {iconDescription} on:click={handleClick} />
